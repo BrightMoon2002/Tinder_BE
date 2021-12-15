c333fbb371f4ce7506a943619b6e57fa49f284ee
Your Name <uefa.euro30@gmail.com>
2021-12-15 11:06:11 +0700

    bang status va da phan quyen

--- src/main/java/com/codegym/model/Status.java
+++ src/main/java/com/codegym/model/Status.java
@@ -1,42 +1,25 @@
 package com.codegym.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
 @Entity
+@Data
 public class Status implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private String name;
+
     @OneToMany(targetEntity = Account.class, mappedBy = "status", cascade = CascadeType.PERSIST)
     @JsonBackReference
     private List<Account> accounts;
 
-    public Status(Long id, List<Account> accounts) {
-        this.id = id;
-        this.accounts = accounts;
-    }
 
-    public Status() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
 }
