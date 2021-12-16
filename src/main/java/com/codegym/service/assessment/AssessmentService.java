67e5782ff0a5c962c3c1b745f426e0c153ff7319
nhimdom200292 <kiemda02@gmail.com>
2021-12-16 14:27:36 +0700

    add service for assessment

---
+++ src/main/java/com/codegym/service/assessment/AssessmentService.java
@@ -0,0 +1,34 @@
+package com.codegym.service.assessment;
+
+import com.codegym.model.receipt.Assessment;
+import com.codegym.repository.assessment.IAssessmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class AssessmentService implements IAssessmentService{
+
+    @Autowired
+    private IAssessmentRepository assessmentRepository;
+
+    @Override
+    public Iterable<Assessment> findAll() {
+        return assessmentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Assessment> findById(Long id) {
+        return assessmentRepository.findById(id);
+    }
+
+    @Override
+    public Assessment save(Assessment assessment) {
+        return assessmentRepository.save(assessment);
+    }
+
+    @Override
+    public void remove(Long id) {
+        assessmentRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/codegym/service/assessment/IAssessmentService.java
@@ -0,0 +1,7 @@
+package com.codegym.service.assessment;
+
+import com.codegym.model.receipt.Assessment;
+import com.codegym.service.IGeneralService;
+
+public interface IAssessmentService extends IGeneralService<Assessment> {
+}
