b638ddf0d8bec214141e97c16b00a699df9078db
nhimdom200292 <kiemda02@gmail.com>
2021-12-16 14:51:37 +0700

    add bill option service

---
+++ src/main/java/com/codegym/service/billOption/BillOptionService.java
@@ -0,0 +1,42 @@
+package com.codegym.service.billOption;
+
+import com.codegym.model.receipt.BillOption;
+import com.codegym.repository.bill_option.IBillOptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BillOptionService implements IBillOptionService{
+
+    @Autowired
+    private IBillOptionRepository billOptionRepository;
+
+    @Override
+    public Iterable<BillOption> findAll() {
+        return billOptionRepository.findAll();
+    }
+
+    @Override
+    public Optional<BillOption> findById(Long id) {
+        return billOptionRepository.findById(id);
+    }
+
+    @Override
+    public BillOption save(BillOption billOption) {
+        return billOptionRepository.save(billOption);
+    }
+
+    @Override
+    public void remove(Long id) {
+        billOptionRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<BillOption> findAll(Pageable pageable) {
+        return billOptionRepository.findAll(pageable);
+    }
+}
---
+++ src/main/java/com/codegym/service/billOption/IBillOptionService.java
@@ -0,0 +1,7 @@
+package com.codegym.service.billOption;
+
+import com.codegym.model.receipt.BillOption;
+import com.codegym.service.IGeneralService;
+
+public interface IBillOptionService extends IGeneralService<BillOption> {
+}
