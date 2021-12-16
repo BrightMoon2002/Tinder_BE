6d73a52b494e8323f45fe8babe82517bbbde84b1
nhimdom200292 <kiemda02@gmail.com>
2021-12-16 14:33:41 +0700

    add find all pageable in bill status

--- src/main/java/com/codegym/service/billstatus/BillStatusService.java
+++ src/main/java/com/codegym/service/billstatus/BillStatusService.java
@@ -3,6 +3,8 @@ package com.codegym.service.billstatus;
 import com.codegym.model.receipt.BillStatus;
 import com.codegym.repository.billstatus.IBillStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -30,5 +32,10 @@ public class BillStatusService implements IBillStatusService {
     @Override
     public void remove(Long id) {
         billStatusRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<BillStatus> findAll(Pageable pageable) {
+        return billStatusRepository.findAll(pageable);
     }
 }
