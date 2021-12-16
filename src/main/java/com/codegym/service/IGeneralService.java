81446a180410e54f8a3e1da01db61b32e4439ff0
nhimdom200292 <kiemda02@gmail.com>
2021-12-16 14:30:34 +0700

    add page findAll in general service

--- src/main/java/com/codegym/service/IGeneralService.java
+++ src/main/java/com/codegym/service/IGeneralService.java
@@ -1,5 +1,8 @@
 package com.codegym.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 
 public interface IGeneralService<T> {
@@ -10,4 +13,6 @@ public interface IGeneralService<T> {
     T save(T t);
 
     void remove(Long id);
+
+    Page<T> findAll(Pageable pageable);
 }
