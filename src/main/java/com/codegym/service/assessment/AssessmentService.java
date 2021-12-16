7af29a2c46fa5d65bd1ee9029af55542294dff20
nhimdom200292 <kiemda02@gmail.com>
2021-12-16 15:14:00 +0700

    add pageable in assessment service

--- src/main/java/com/codegym/service/assessment/AssessmentService.java
+++ src/main/java/com/codegym/service/assessment/AssessmentService.java
@@ -3,6 +3,8 @@ package com.codegym.service.assessment;
 import com.codegym.model.receipt.Assessment;
 import com.codegym.repository.assessment.IAssessmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -30,5 +32,10 @@ public class AssessmentService implements IAssessmentService{
     @Override
     public void remove(Long id) {
         assessmentRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Assessment> findAll(Pageable pageable) {
+        return assessmentRepository.findAll(pageable);
     }
 }
