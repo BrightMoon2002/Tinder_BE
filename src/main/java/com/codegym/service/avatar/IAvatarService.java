0cca6297d1851ac1975e836f84ee4efc1097d23a
hieuhvh1994 <hieudaohn94@gmail.com>
2021-12-16 21:42:58 +0700

    Added some repositories and services

---
+++ src/main/java/com/codegym/service/avatar/IAvatarService.java
@@ -0,0 +1,7 @@
+package com.codegym.service.avatar;
+
+import com.codegym.model.user.Avatar;
+import com.codegym.service.IGeneralService;
+
+public interface IAvatarService extends IGeneralService<Avatar> {
+}
---
+++ src/main/java/com/codegym/service/checker/CheckerService.java
@@ -0,0 +1,42 @@
+package com.codegym.service.checker;
+
+import com.codegym.model.user.Checker;
+import com.codegym.repository.checker.ICheckerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CheckerService implements ICheckerService{
+
+    @Autowired
+    ICheckerRepository checkerRepository;
+
+    @Override
+    public Iterable<Checker> findAll() {
+        return checkerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Checker> findById(Long id) {
+        return checkerRepository.findById(id);
+    }
+
+    @Override
+    public Checker save(Checker checker) {
+        return checkerRepository.save(checker);
+    }
+
+    @Override
+    public void remove(Long id) {
+        checkerRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Checker> findAll(Pageable pageable) {
+        return checkerRepository.findAll(pageable);
+    }
+}
