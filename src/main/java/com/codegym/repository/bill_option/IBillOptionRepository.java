c1e5a6af14b3c58773d7bff630c72509daf07881
nhimdom200292 <kiemda02@gmail.com>
2021-12-16 14:07:59 +0700

    add repo for bill option

---
+++ src/main/java/com/codegym/repository/bill_option/IBillOptionRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.repository.bill_option;
+
+import com.codegym.model.receipt.BillOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBillOptionRepository extends JpaRepository<BillOption, Long> {
+}
